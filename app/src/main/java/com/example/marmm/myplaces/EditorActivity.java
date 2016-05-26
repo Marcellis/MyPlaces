5659166b64df49574d06fdb19d9864c2e691400e
Marcellis <m.m.c.m.marcellis@hva.nl>
2016-05-26 21:52:33 +0200

    Key added

--- app/src/main/java/com/example/marmm/myplaces/EditorActivity.java
+++ app/src/main/java/com/example/marmm/myplaces/EditorActivity.java
@@ -4,20 +4,21 @@ import android.Manifest;
 import android.content.ContentValues;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.location.Address;
 import android.location.Geocoder;
-import android.location.Location;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -64,6 +65,7 @@ public class EditorActivity extends AppCompatActivity implements OnMapReadyCallb
 
         Intent intent = getIntent();
         uri = intent.getParcelableExtra(LocationsProvider.CONTENT_ITEM_TYPE);
+        populateMap();
     }
 
 
@@ -82,9 +84,48 @@ public class EditorActivity extends AppCompatActivity implements OnMapReadyCallb
         mMap.getUiSettings().setRotateGesturesEnabled(true);
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
             mMap.setMyLocationEnabled(true);
-        mMap.getUiSettings().setZoomControlsEnabled(true);
+         mMap.getUiSettings().setZoomControlsEnabled(true);
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setMapToolbarEnabled(true);
+
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+            @Override
+            public void onMarkerDragStart(Marker marker) {
+                marker.remove();
+            }
+
+            @Override
+            public void onMarkerDrag(Marker marker) {
+            }
+
+            @Override
+            public void onMarkerDragEnd(Marker marker) {
+            }
+        });
+// add new marker on long click
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+                addMarker(latLng);
+                // set the LatLng values each time a new marker is created
+                location.setLatitude(marker.getPosition().latitude);
+                location.setLongitude(marker.getPosition().longitude);
+                location.setCity(showCityname(location.getLatitude(), location.getLongitude()));
+            }
+        });
+
+        if (oldLatitude != 0.0 && oldLongitude != 0.0){
+            // add marker and animate camera
+            LatLng latLng = new LatLng(oldLatitude,oldLongitude);
+            addMarker(latLng);
+            CameraUpdate markerLocation = CameraUpdateFactory.newLatLngZoom(latLng, 15);
+            mMap.animateCamera(markerLocation);
+        } else {
+            Toast.makeText(getApplicationContext(), "Unable to mark location.", Toast.LENGTH_LONG).show();
+            LatLng latLng = new LatLng(52.092876,5.104480);
+            CameraUpdate markerLocation = CameraUpdateFactory.newLatLngZoom(latLng, 6);
+            mMap.animateCamera(markerLocation);
+        }
     }
 
     private String showCityname(double latitude, double longitude){
@@ -138,7 +179,7 @@ public class EditorActivity extends AppCompatActivity implements OnMapReadyCallb
         setResult(RESULT_OK);
     }
 
-    private void insertLocation(Location location) {
+    private void insertLocation(LocationModel location) {
         ContentValues values = new ContentValues();
         values.put(DBOpenHelper.LOCATION_NAME, location.getName());
         values.put(DBOpenHelper.LOCATION_CITY, location.getCity());
@@ -148,4 +189,76 @@ public class EditorActivity extends AppCompatActivity implements OnMapReadyCallb
         Toast.makeText(EditorActivity.this, "location added", Toast.LENGTH_SHORT).show();
         setResult(RESULT_OK);
     }
+
+    private void finishEditing() {
+        // get the string from the editText and set it in the object
+        String name = locationName.getText().toString().trim();
+        location.setName(name);
+
+        switch (action) {
+            case Intent.ACTION_INSERT:
+                if (name.length() == 0) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    insertLocation(location);
+                }
+                break;
+            case Intent.ACTION_EDIT:
+                if (name.length() == 0) {
+                    deleteLocation();
+                } else if (oldName.equals(location.getName()) && (oldLatitude == location.getLatitude() || oldLongitude == location.getLongitude() )) {
+                    setResult(RESULT_CANCELED);
+                } else {
+                    updateLocation(location);
+                }
+        }
+        finish();
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.action_delete:
+                deleteLocation();
+                break;
+            case R.id.action_done:
+                finishEditing();
+                break;
+        }
+        return true;
+    }
+
+    private void populateMap(){
+        location = new LocationModel();
+        // If the uri is NULL, the activity is setup empty and seen as a new location
+        if (uri == null) {
+            action = Intent.ACTION_INSERT;
+            setTitle("New location");
+        } // If the uri is not NULL, the activity is loaded with the data from the database
+        else {
+            setTitle("Edit location");
+            action = Intent.ACTION_EDIT;
+            locationFilter = DBOpenHelper.LOCATION_ID + "=" + uri.getLastPathSegment();
+            Cursor cursor = getContentResolver().query(uri, DBOpenHelper.ALL_COLUMNS, locationFilter, null, null);
+            cursor.moveToFirst();
+
+            // get the old values from the database
+            oldName = cursor.getString(cursor.getColumnIndex(DBOpenHelper.LOCATION_NAME));
+            oldCity = cursor.getString(cursor.getColumnIndex(DBOpenHelper.LOCATION_CITY));
+            oldLatitude = cursor.getDouble(cursor.getColumnIndex(DBOpenHelper.LOCATION_LATITUDE));
+            oldLongitude = cursor.getDouble(cursor.getColumnIndex(DBOpenHelper.LOCATION_LONGITUDE));
+
+            // Set the old values in the view
+            locationName.setText(oldName);
+            // if Latlng is not set again, take the old values.
+            location.setLongitude(oldLongitude);
+            location.setLatitude(oldLatitude);
+            location.setCity(oldCity);
+        }
+    }
+
 }
